4c97a892c53b72fdc8c8638fe29c9b63fc43b442
Sarthak Gupta <gsarthak810@gmail.com>
2026-07-06 17:41:18 +0530

    Add JWT verification endpoint

--- app.py
+++ app.py
@@ -3,15 +3,32 @@ import uuid
 from fastapi import FastAPI, Query
 from fastapi.middleware.cors import CORSMiddleware
 from starlette.middleware.base import BaseHTTPMiddleware
+import jwt
+from fastapi.responses import JSONResponse
 
+from fastapi import HTTPException
+from pydantic import BaseModel
 app = FastAPI()
 
+PUBLIC_KEY = """
+-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA2okOHspNjgA+2rTLbeuY
+cxiP/hG8C6Sb9iwg3yiLAA4HCnpITcbWCSelbvbYGuc3EbNy4xFyf5Cbj5DHJMID
+EkryOgyd2giIIIBOUBj8S63uGcnRpOBh9NFatfNwheKuzsPuVNldu6A9cNteNpXc
+WyJjG2axVfmq7i6SuKr1JoWYG7xTTAvKPujSl4OtsQfO3h5NepzdfXpr28oNnzfW
+ed+zclR6BcmNNo/WVfJ4xyCLSf0BCOgdTgW6PdaChd1l9VDetJZVEgC5tkyvXsfI
+SI6iyrYbKR0NEBSqq4XkadEjsCs4F1RncsS4LlgniT7GlkL9Mce3b0wGLs9/7ZIX
+dQIDAQAB
+-----END PUBLIC KEY-----
+"""
 # ------------------------------------
 # Allowed Origin
 # ------------------------------------
 
 ALLOWED_ORIGIN = "https://dash-6ikh0r.example.com"
 
+class TokenRequest(BaseModel):
+    token: str
 # ------------------------------------
 # CORS
 # ------------------------------------
@@ -75,3 +92,31 @@ async def stats(values: str = Query(...)):
         "max": maximum,
         "mean": mean
     }
+
+@app.post("/verify")
+async def verify(request: TokenRequest):
+
+    try:
+
+        payload = jwt.decode(
+            request.token,
+            PUBLIC_KEY,
+            algorithms=["RS256"],
+            audience="tds-wqrasmzc.apps.exam.local",
+            issuer="https://idp.exam.local"
+        )
+
+        return {
+            "valid": True,
+            "email": payload.get("email"),
+            "sub": payload.get("sub"),
+            "aud": payload.get("aud")
+        }
+
+    except Exception:
+        return JSONResponse(
+        status_code=401,
+        content={
+            "valid": False
+        }
+    )
